1e32743d6877b4c378039083f1e5368d947448cf
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-30 22:03:24 +0600

    data-show

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,23 @@
 import React from "react";
-import "./Product.css"
+import "./Product.css";
 
 const Product = (props) => {
-	const { product, color, price } = props.product;
+	const { product, color, price, name, email, phone, id } = props.product;
+
+	const addToCart = (id) => {
+		console.log(id);
+	};
 	return (
-		<div className="product">
-			<h3>Product Name: {product}</h3>
-			<p>Price: {price}$</p>
-			<p>Color: {color}</p>
+		<div className='product'>
+			<h2>Product Name: {product}</h2>
+			<h3>Price: {price}$</h3>
+			<h3>Color: {color}</h3>
+			<h3>Seller Contact Info</h3>
+			<p>Name: {name}</p>
+			<p>Phone: {phone}</p>
+			<p>Email: {email}</p>
+			<p>Email: {id}</p>
+			<button onClick={() => addToCart(id)}>Purchase.</button>
 		</div>
 	);
 };
